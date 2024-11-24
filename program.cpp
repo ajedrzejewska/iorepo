86136ca5e497f0eda302419bb9cfc8e49e2c3edf
ajedrzejewska <322894@stud.umk.pl>
2024-11-24 18:34:10 +0100

    Dodano sprawdzenie czy liczba nie jest ujemna

--- program.cpp
+++ program.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int fib(int n)
+{
+	if(n<=1)
+	{
+		return n;
+	}
+	return fib(n-1)+fib(n-2);
+}
+
 int main() {
     int liczba;
     cin >> liczba;
+    if(liczba<0)
+    {
+	    cout<<"Podano ujemna liczbe!"<<endl;
+	    return 0;
+    }
     cout << "Podana liczba: " << liczba << endl;
+    cout << "Wartosc (Fibonacci): "<<fib(liczba)<<endl;
     return 0;
 }
 
